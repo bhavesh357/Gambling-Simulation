d3461687e88a5608e404207630d8da54e2f9abe3
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-01 22:15:42 +0530

    ADDED: player plays till he reches target or stoploss

--- gambler.sh
+++ gambler.sh
@@ -1,8 +1,11 @@
+#!/bin/bash -x
 #constants
 BETTING_AMOUNT=1
 
 #variable
 stake=100
+target=0
+stoploss=0
 
 function bet() {
 	if [ $(($RANDOM%2)) -eq 0 ]
@@ -13,5 +16,15 @@ function bet() {
 	fi
 }
 
+function setTargetStoploss() {
+	target=$(($stake+$(($stake/2))))
+	stoploss=$(($stake-$(($stake/2))))
+}
+
 echo "Welcome to Gambling"
-bet()
+setTargetStoploss
+while [[ $stake -ne $target && $stake -ne $stoploss ]]
+do
+	bet
+done
+echo $stake
