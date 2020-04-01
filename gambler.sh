d0bd8fd1b034acd3c0f7738796db7508bc013800
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-01 21:59:49 +0530

    MODIFIED: bet function to simulate betting with equal possibility

--- gambler.sh
+++ gambler.sh
@@ -5,7 +5,12 @@ BETTING_AMOUNT=1
 stake=100
 
 function bet() {
-	
+	if [ $(($RANDOM%2)) -eq 0 ]
+	then
+		stake=$(($stake-$BETTING_AMOUNT))
+	else
+		stake=$(($stake+$BETTING_AMOUNT))
+	fi
 }
 
 echo "Welcome to Gambling"
