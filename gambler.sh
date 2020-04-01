ea41d7dd26b2b531c0f7176dc270bbb430f30a48
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-01 22:26:03 +0530

    ADDED: player plays for 20 days and it reports winning or losing amount

--- gambler.sh
+++ gambler.sh
@@ -6,6 +6,7 @@ BETTING_AMOUNT=1
 stake=100
 target=0
 stoploss=0
+declare -a gamblingDays
 
 function bet() {
 	if [ $(($RANDOM%2)) -eq 0 ]
@@ -21,10 +22,27 @@ function setTargetStoploss() {
 	stoploss=$(($stake-$(($stake/2))))
 }
 
+function checkReport() {
+	if [ $stake -gt 100 ]
+	then
+		echo you have won $(($stake-100))
+	fi
+	if [ $stake -lt 100 ]
+	then
+		echo you have lost $((100-$stake))
+	else
+		echo you have not won anything
+	fi
+}
+
 echo "Welcome to Gambling"
-setTargetStoploss
-while [[ $stake -ne $target && $stake -ne $stoploss ]]
-do
-	bet
+for((i=1;i<21;i++))
+do	
+	setTargetStoploss
+	while [[ $stake -ne $target && $stake -ne $stoploss ]]
+	do
+		bet
+	done
 done
+checkReport
 echo $stake
